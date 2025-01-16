7470708ecf23d6705059a8276cd8be203d838b8a
Sidharth Rajagopal <rsidharth53@gmail.com>
2025-01-15 20:50:24 -0800

    added timestamps, headings, and imu roll and pitch

--- src/car_odom_publisher.py
+++ src/car_odom_publisher.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from geometry_msgs.msg import PoseStamped, Twist, PoseWithCovariance, TwistWithCovariance
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
 import numpy as np
-from tf.transformations import quaternion_matrix
+from tf.transformations import quaternion_matrix, euler_from_quaternion, quaternion_from_euler
 
 class OdomPublisher:
     def __init__(self, alpha=0.2):
@@ -18,8 +19,14 @@ class OdomPublisher:
         self.pub_rate = 100.0
 
         self.odom_pub = rospy.Publisher("car_odom", Odometry, queue_size=1)
-        rospy.Subscriber("car_pose", PoseStamped, self.pose_callback)
+        self.pose_pub = rospy.Publisher("car_pose", PoseStamped, queue_size=1)
+        
+        rospy.Subscriber("mocap_pose", PoseStamped, self.pose_callback)
+        rospy.Subscriber("imu/data", Imu, self.imu_callback)
+
+        self.imu_ori = [0.0,0.0,0.0]
         self.odom_msg = Odometry()
+        self.pose_msg = PoseStamped()
         self.update_odom = False
         self.main_loop()
 
@@ -28,11 +35,22 @@ class OdomPublisher:
         while not rospy.is_shutdown():
             if self.update_odom:
                 self.odom_pub.publish(self.odom_msg)
+                self.pose_pub.publish(self.pose_msg)
+                self.update_odom = False
             rate.sleep()
 
+    def imu_callback(self, msg):
+        quaternion = (
+            msg.orientation.x,
+            msg.orientation.y,
+            msg.orientation.z,
+            msg.orientation.w
+        )
+        self.imu_ori = euler_from_quaternion(quaternion)
+    
     def pose_callback(self, msg):
         current_time = rospy.Time.now()
-        
+
         # If we have a previous pose, calculate velocity
         if self.last_pose is not None and self.last_time is not None:
             dt = (current_time - self.last_time).to_sec()
@@ -46,7 +64,25 @@ class OdomPublisher:
 
                 # Convert velocity to body frame
                 orientation = msg.pose.orientation
-                quat = [orientation.x, orientation.y, orientation.z, orientation.w]
+                quaternion = (
+                    orientation.x,
+                    orientation.y,
+                    orientation.z,
+                    orientation.w
+                )
+                #use imu for roll and pitch
+                rpy = euler_from_quaternion(quaternion)
+                ori = [0,0,0]
+                ori[0] = self.imu_ori[0]
+                ori[1] = self.imu_ori[1]
+                ori[2] = rpy[2]
+
+                quat = quaternion_from_euler(*ori, 'sxyz')
+                msg.pose.orientation.x = quat[0]
+                msg.pose.orientation.y = quat[1]
+                msg.pose.orientation.z = quat[2]
+                msg.pose.orientation.w = quat[3]
+
                 rotation_matrix = quaternion_matrix(quat)[:3, :3]
                 velocity_body = np.dot(rotation_matrix.T, velocity_global)
 
@@ -54,17 +90,27 @@ class OdomPublisher:
                 self.filtered_velocity = (
                     self.alpha * velocity_body + (1 - self.alpha) * self.filtered_velocity
                 )
-
                 # Publish the filtered velocity
                 twist_msg = Twist()
                 twist_msg.linear.x = self.filtered_velocity[0]
                 twist_msg.linear.y = self.filtered_velocity[1]
                 twist_msg.linear.z = self.filtered_velocity[2]
 
+                timestamp = msg.header.stamp
+
+                # Update the timestamp for each message
+                self.odom_msg.header.stamp = timestamp
+                self.odom_msg.header.frame_id = "map"  # or any relevant frame_id
+                self.odom_msg.child_frame_id = "base_link"
+                self.pose_msg.header.stamp = timestamp
+                self.pose_msg.header.frame_id = "base_link"  # or any relevant frame_id
+                self.pose_msg.pose = msg.pose
+
                 twist_covar_msg = TwistWithCovariance()
                 twist_covar_msg.twist = twist_msg
                 pos_covar_msg = PoseWithCovariance()
                 pos_covar_msg.pose = msg.pose
+
                 self.odom_msg.pose = pos_covar_msg
                 self.odom_msg.twist = twist_covar_msg
                 self.update_odom = True
